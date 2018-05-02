897dd1b98924f7cbe617074c6821fbcf80a9a207
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:39:28 -0400

    Modernized syntax

--- jQuery/Simple Lightbox/js/app.js
+++ jQuery/Simple Lightbox/js/app.js
@@ -9,9 +9,9 @@
 //Problem: User when clicking on images goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+const $overlay = $('<div id="overlay"></div>');
+const $image = $("<img>");
+const $caption = $("<p></p>");
 
 //1. Add an image to overlay
 $overlay.append($image);
@@ -27,18 +27,18 @@ $("body").append($overlay);
 //3. Capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
   event.preventDefault();
-  var imageLocation = $(this).attr("href");
+  const imageLocation = $(this).attr("href");
   //3.1 Update overlay with the image linked in the link
   $image.attr("src", imageLocation);
   //3.2 Show the overlay
   $overlay.show();
   //3.3 Get child's alt attribute and set caption
-  var captionText = $(this).children("img").attr("alt");
+  const captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
 });
 
 //4. When overlay is clicked
-$overlay.click(function(){
+$overlay.click(() => {
   //4.1 Hide the overlay
   $overlay.hide();
 });
