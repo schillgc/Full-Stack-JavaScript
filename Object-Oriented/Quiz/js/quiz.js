d7435d4d614af62077e63752933b5edb485e79eb
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:51:47 -0400

    Modernized syntax

--- Object-Oriented/Quiz/js/quiz.js
+++ Object-Oriented/Quiz/js/quiz.js
@@ -6,23 +6,25 @@
 * 
 */
 
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.currentQuestionIndex = 0;
-}
-
-Quiz.prototype.guess = function(answer) {
-  if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
-    this.score++;
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
   }
-  this.currentQuestionIndex++;
-};
 
-Quiz.prototype.getCurrentQuestion = function() {
-  return this.questions[this.currentQuestionIndex];
-};
+  guess(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.currentQuestionIndex++;
+  }
 
-Quiz.prototype.hasEnded = function() {
-  return this.currentQuestionIndex >= this.questions.length;
-};
+  getCurrentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+}
