73f10cb0f2783f0c28ccd1d8c1fe066afe027378
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 05:27:06 -0400

    Modernized syntax

--- MEAN Stack/Node JS/Treehouse API/Simple Dynamic Website/js/renderer.js
+++ MEAN Stack/Node JS/Treehouse API/Simple Dynamic Website/js/renderer.js
@@ -1,20 +1,10 @@
-/*
-* renderer js
-*
-* Copyright (c) 2016 Gavin Schilling Marketing
-* Licensed under the GNU Affero General Public License
-* 
-*/
-
-'use strict';
-
-const fs = require('fs');
+import fs from 'fs';
 
 function mergeValues(values, content) {
 	// Cycle over the keys
-	for(var key in values) {
+	for(const key in values) {
 		// Replace all {{keys}} with the value from the values object
-		content = content.replace("{{" + key + "}}", values[key]);
+		content = content.replace(`{{${key}}}`, values[key]);
 	}
 	// Return merged content
 	return content;
@@ -22,11 +12,11 @@ function mergeValues(values, content) {
 
 function view(templateName, values, response) {
 	// Read from the template file
-	var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf-8"});
+	let fileContents = fs.readFileSync(`./views/${templateName}.html`, {encoding: "utf-8"});
 	// Insert values into the content
 	fileContents = mergeValues(values, fileContents);
 	// Write out the contents to the response
 	response.write(fileContents);
 }
 
-module.exports.view = view;
+export {view};
