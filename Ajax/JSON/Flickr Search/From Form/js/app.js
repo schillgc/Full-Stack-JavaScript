3f7520bc1682dfa580ef11c193fcb9141aa03f95
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:25:24 -0400

    Modernized syntax

--- Ajax/JSON/Flickr Search/From Form/js/app.js
+++ Ajax/JSON/Flickr Search/From Form/js/app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*
 * app js
 *
@@ -8,28 +6,28 @@
 * 
 */
 
-$(document).ready(function() {
-  $("form").submit(function(event) {
+$(document).ready(() => {
+  $("form").submit(event => {
     event.preventDefault();
-    var $submitButton = $("#submit");
-    var $searchField = $("#search");
+    const $submitButton = $("#submit");
+    const $searchField = $("#search");
 
     $searchField.prop("disabled", true);
     $submitButton.attr("disabled", true).val("Searching ...");
     // the AJAX part
-    var flickerAPI =
+    const flickerAPI =
       "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var flickrOptions = {
+    const flickrOptions = {
       tags: $searchField.val(),
       format: "json"
     };
 
     function displayPhotos(data) {
-      var photoHTML = "<ul>";
-      $.each(data.items, function(i, photo) {
+      let photoHTML = "<ul>";
+      $.each(data.items, (i, photo) => {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        photoHTML += `<a href="${photo.link}" class="image">`;
+        photoHTML += `<img src="${photo.media.m}"></a></li>`;
       }); // end each
       photoHTML += "</ul>";
       $("#photos").html(photoHTML);
