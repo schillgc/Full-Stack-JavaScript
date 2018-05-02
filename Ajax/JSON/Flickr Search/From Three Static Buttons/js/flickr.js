734c18f94d23acdeb9a74f807a11f51fbd51d471
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 03:42:17 -0400

    Modernized syntax

--- Ajax/JSON/Flickr Search/From Three Static Buttons/js/flickr.js
+++ Ajax/JSON/Flickr Search/From Three Static Buttons/js/flickr.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 * flickr js
 *
@@ -6,11 +8,12 @@
 * 
 */
 
-$(document).ready(function () {
-  $('button').click(function () {
+$(document).ready(function() {
+  $("button").click(function() {
     $("button").removeClass("selected");
     $(this).addClass("selected");
-    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickrAPI =
+      "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var animal = $(this).text();
     var flickrOptions = {
       tags: animal,
@@ -18,7 +21,7 @@ $(document).ready(function () {
     }; // end flickrOptions
     function displayPhotos(data) {
       var photoHTML = "<ul>";
-      $.each(data.items, function (i, photo) {
+      $.each(data.items, function(i, photo) {
         photoHTML += "<li class='grid-25 tablet-grid-50'>";
         photoHTML += "<a href='" + photo.link + "' class='image'>";
         photoHTML += "<img src='" + photo.media.m + "'></a></li>";
