d9161463db3ca933f6f1a55ae3f47068ba4a0b1e
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:20:57 -0400

    Modernized syntax

--- Ajax/JSON/Office Status Intranet/js/widget.js
+++ Ajax/JSON/Office Status Intranet/js/widget.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*
 * widget js
 *
@@ -8,30 +6,30 @@
 * 
 */
 
-$(document).ready(function() {
-  $.getJSON("../data/employees.json", function(data) {
-    var statusHTML = '<ul class="bulleted">';
-    $.each(data, function(index, employee) {
+$(document).ready(() => {
+  $.getJSON("../data/employees.json", data => {
+    let statusHTML = '<ul class="bulleted">';
+    $.each(data, (index, employee) => {
       if (employee.inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
         statusHTML += '<li class="out">';
       }
-      statusHTML += employee.name + "</li>";
+      statusHTML += `${employee.name}</li>`;
     }); //end each
     statusHTML += "</ul>";
     $("#employeeList").html(statusHTML);
   }); // end getJSON
 
-  $.getJSON("../data/rooms.json", function(data) {
-    var statusHTML = '<ul class="rooms">';
-    $.each(data, function(index, room) {
+  $.getJSON("../data/rooms.json", data => {
+    let statusHTML = '<ul class="rooms">';
+    $.each(data, (index, room) => {
       if (room.available === true) {
         statusHTML += '<li class="empty">';
       } else {
         statusHTML += '<li class="full">';
       }
-      statusHTML += room.room + "</li>";
+      statusHTML += `${room.room}</li>`;
     }); //end each
     statusHTML += "</ul>";
     $("#roomList").html(statusHTML);
