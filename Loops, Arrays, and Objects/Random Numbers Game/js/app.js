47e69930368e452f831cd322f4e3a01aaa00ea67
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 05:16:11 -0400

    Modernized syntax

--- Loops, Arrays, and Objects/Random Numbers Game/js/app.js
+++ Loops, Arrays, and Objects/Random Numbers Game/js/app.js
@@ -6,10 +6,10 @@
 * 
 */
 
-var upper = 10000;
-var randomNumber = getRandomNumber (upper);
-var guess;
-var attempts = 0;
+const upper = 10000;
+const randomNumber = getRandomNumber (upper);
+let guess;
+let attempts = 0;
 
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
@@ -19,5 +19,5 @@ while ( guess !== randomNumber ) {
   guess = getRandomNumber ( upper );
   attempts += 1;
 }
-document.write("<p>The random number was: " + randomNumber + "</p>");
-document.write("<p>It took the computer " + attempts + " attempts to get it right.</p>");
+document.write(`<p>The random number was: ${randomNumber}</p>`);
+document.write(`<p>It took the computer ${attempts} attempts to get it right.</p>`);
