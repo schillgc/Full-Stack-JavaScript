528b49599855be418de0ae3d3d78bfb0da6aadac
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:57:08 -0400

    Modernized syntax

--- Object-Oriented/Quiz/js/app.js
+++ Object-Oriented/Quiz/js/app.js
@@ -7,14 +7,14 @@
 */
 
 //Creates Questions
-var questions = [
+const questions = [
   new Question("Who was the first United States president?", [ "President George Washington", "President Thomas Jefferson" ], "President George Washington"),
   new Question("Where was the first capital city of the United States?", [ "Philadelphia", "Washington DC" ], "Philadelphia"),
   new Question("What country did Christopher Columbus believe he landed in 1492 upon the Mayflower?", [ "The United States of America", "India" ], "India")
 ];
 
 // Creates Quiz
-var quiz = new Quiz(questions);
+const quiz = new Quiz(questions);
 
 // Displays Quiz
 QuizUI.displayNext();
