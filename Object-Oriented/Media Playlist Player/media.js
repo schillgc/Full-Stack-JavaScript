09e57eae0e7bc690177980b906b6fefa52d5fb75
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:59:06 -0400

    Modernized syntax

--- Object-Oriented/Media Playlist Player/media.js
+++ Object-Oriented/Media Playlist Player/media.js
@@ -6,16 +6,18 @@
 * 
 */
 
-function Media(title, duration) {
-  this.title = title;
-  this.duration = duration;
-  this.isPlaying = false;
+class Media {
+  constructor(title, duration) {
+    this.title = title;
+    this.duration = duration;
+    this.isPlaying = false;
+  }
+
+  play() {
+    this.isPlaying = true;
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
 }
-
-Media.prototype.play = function() {
-  this.isPlaying = true;
-};
-
-Media.prototype.stop = function() {
-  this.isPlaying = false;
-};
