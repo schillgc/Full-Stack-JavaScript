523cfc9340f3d880422a65078684e74512b7d84d
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 03:37:59 -0400

    Modernized syntax

--- Ajax/JSON/Flickr Search/From Form/js/app.js
+++ Ajax/JSON/Flickr Search/From Form/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 * app js
 *
@@ -7,36 +9,33 @@
 */
 
 $(document).ready(function() {
-
-
- $('form').submit(function (event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-    var $submitButton = $('#submit');
-    var $searchField = $('#search');
+    var $submitButton = $("#submit");
+    var $searchField = $("#search");
 
     $searchField.prop("disabled", true);
-    $submitButton.attr("disabled", true).val("Searching ..."); 
+    $submitButton.attr("disabled", true).val("Searching ...");
     // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickerAPI =
+      "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var flickrOptions = {
       tags: $searchField.val(),
       format: "json"
     };
-   
+
     function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
+      var photoHTML = "<ul>";
+      $.each(data.items, function(i, photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       }); // end each
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
+      photoHTML += "</ul>";
+      $("#photos").html(photoHTML);
       $searchField.prop("disabled", false);
       $submitButton.attr("disabled", false).val("Search");
     }
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
-
   }); // end click
-
 }); // end ready
