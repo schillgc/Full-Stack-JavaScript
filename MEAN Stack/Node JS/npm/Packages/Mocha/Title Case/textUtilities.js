5ee15179e7d23d5e36e1b9886d2245ef00a5ecef
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 05:42:26 -0400

    Modernized syntax

--- MEAN Stack/Node JS/npm/Packages/Mocha/Title Case/textUtilities.js
+++ MEAN Stack/Node JS/npm/Packages/Mocha/Title Case/textUtilities.js
@@ -1,14 +1,12 @@
-var expect = require('chai').expect;
+import {expect} from 'chai';
 
 expect(true).to.be.true;
 
 function titleCase (title) {
-	var words = title.split (' ');
-	var titleCasedWords = words.map (function (word) {
-		return word[0].toUpperCase () + word.substring (1);
-	});
+	const words = title.split (' ');
+	const titleCasedWords = words.map (word => word[0].toUpperCase () + word.substring (1));
 	return titleCasedWords.join (' ');
-};
+}
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.equal('A');
