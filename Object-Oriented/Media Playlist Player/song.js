b0e512a15351cefa5737b4c28d5064a725ac012a
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 05:03:05 -0400

    Modernized syntax

--- Object-Oriented/Media Playlist Player/song.js
+++ Object-Oriented/Media Playlist Player/song.js
@@ -6,24 +6,26 @@
 * 
 */
 
-function Song(title, artist, duration) {
-  Media.call(this, title, duration);
-  this.artist = artist;
+class Song {
+  constructor(title, artist, duration) {
+    Media.call(this, title, duration);
+    this.artist = artist;
+  }
+
+  toHTML() {
+    let htmlString = '<li';
+    if (this.isPlaying) {
+      htmlString += 'class="current"';
+    }
+    htmlString += '>';
+    htmlString += this.title;
+    htmlString += ' - '
+    htmlString += this.artist;
+    htmlString += '<span class="duration">'
+    htmlString += this.duration;
+    htmlString += '</span></li>';
+    return htmlString;
+  }
 }
 
 Song.prototype = Object.create(Media.prototype);
-
-Song.prototype.toHTML = function() {
-  var htmlString = '<li';
-  if (this.isPlaying) {
-    htmlString += 'class="current"';
-  }
-  htmlString += '>';
-  htmlString += this.title;
-  htmlString += ' - '
-  htmlString += this.artist;
-  htmlString += '<span class="duration">'
-  htmlString += this.duration;
-  htmlString += '</span></li>';
-  return htmlString;
-};
