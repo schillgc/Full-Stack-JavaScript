e42bdb8033cd3811835a63a2b7eea810768cfa6c
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-02 04:55:20 -0400

    Modernized syntax

--- Object-Oriented/Quiz/js/question.js
+++ Object-Oriented/Quiz/js/question.js
@@ -6,12 +6,14 @@
 * 
 */
 
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
-}
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
 
-Question.prototype.isCorrectAnswer = function (choice) {
-  return this.answer === choice;
-};
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
