1bbef1aad31eefffdca39e02818ea746c9eca4d7
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-03 00:37:04 -0400

    Modernized syntax

--- MEAN Stack/Node JS/npm/Packages/Gulp JS/advanced-gulp-example-master/gulp/markups.js
+++ MEAN Stack/Node JS/npm/Packages/Gulp JS/advanced-gulp-example-master/gulp/markups.js
@@ -1,20 +1,18 @@
-'use strict';
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
+const $ = require('gulp-load-plugins')();
 
-var $ = require('gulp-load-plugins')();
-
-module.exports = function(options) {
-  gulp.task('markups', function() {
+export default options => {
+  gulp.task('markups', () => {
     function renameToHtml(path) {
       path.extname = '.html';
     }
 
-    return gulp.src(options.src + '/app/**/*.jade')
+    return gulp.src(`${options.src}/app/**/*.jade`)
       .pipe($.consolidate('jade', { basedir: options.src, doctype: 'html', pretty: '  ' })).on('error', options.errorHandler('Jade'))
       .pipe($.rename(renameToHtml))
-      .pipe(gulp.dest(options.tmp + '/serve/app/'))
-      .pipe(browserSync.reload({ stream: true }));
+      .pipe(gulp.dest(`${options.tmp}/serve/app/`))
+      .pipe(browserSync.reload({ stream: true }));
   });
 };
