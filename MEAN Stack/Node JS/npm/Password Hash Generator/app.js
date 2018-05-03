65702cb44b922de87090b4cf9abacff29ae80fc5
Gavin Schilling <schillgc@users.noreply.github.com>
2018-05-03 01:05:11 -0400

    Modernized syntax

--- MEAN Stack/Node JS/npm/Password Hash Generator/app.js
+++ MEAN Stack/Node JS/npm/Password Hash Generator/app.js
@@ -6,12 +6,12 @@
 * 
 */
 
-var unsecurePlainTextPassword = "password";
+const unsecurePlainTextPassword = "password";
 
-var colors = require('colors');
-var bcrypt = require('bcrypt');
-bcrypt.genSalt(10, function(err, salt) {
-  bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
+import colors from 'colors';
+import bcrypt from 'bcrypt';
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(unsecurePlainTextPassword, salt, (err, hash) => {
   // Store hash in your password DB.
   console.log(hash.green);
   });
